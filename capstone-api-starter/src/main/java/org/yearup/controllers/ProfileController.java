853ca37e62089ab6d6d1bb6a01f0a5bd630c6f76
Corey <codeancrazy9@gmail.com>
2026-06-25 20:07:32 -0400

    added - update and findById to profileService

--- capstone-api-starter/src/main/java/org/yearup/controllers/ProfileController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,4 +1,30 @@
 package org.yearup.controllers;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.models.Profile;
+import org.yearup.service.ProfileService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("profiles")
+@CrossOrigin
 public class ProfileController {
+    private ProfileService profileService;
+
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Profile>> getAll(){
+        List<Profile> profiles = profileService.getAll();
+        return new ResponseEntity<>(profiles, HttpStatus.OK);
+    }
 }
--- capstone-api-starter/src/main/java/org/yearup/service/ProfileService.java
+++ capstone-api-starter/src/main/java/org/yearup/service/ProfileService.java
@@ -13,9 +13,29 @@ public class ProfileService
     {
         this.profileRepository = profileRepository;
     }
-
+    //given
     public Profile create(Profile profile)
     {
         return profileRepository.save(profile);
     }
+    //update
+    public Profile update(int userId, Profile profile) {
+        //update profile and return the updated profile
+        Profile updating = profileRepository.findById(userId).orElseThrow();
+        updating.setFirstName(profile.getFirstName());
+        updating.setLastName(profile.getLastName());
+        updating.setPhone(profile.getPhone());
+        updating.setEmail(profile.getEmail());
+        updating.setAddress(profile.getAddress());
+        updating.setCity(profile.getCity());
+        updating.setState(profile.getState());
+        updating.setZip(profile.getZip());
+        return profileRepository.save(updating);
+    }
+    //getById
+    public Profile getById(int userId){
+        //get category by id
+        return profileRepository.findById(userId).orElse(null);
+    }
+
 }
